10d852b6c0ce22447597fe9834d36c0e83c6edb2
Szabolcs Kopanyi <sz.kopanyi@gmail.com>
2020-03-11 13:24:30 +0100

    boom

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -72,6 +72,13 @@ def is_full(board):
 
 
 def print_board(board):
+    print(    1   2   3 
+
+A   board[0][0] | board[0][1] | board[0][2] 
+   ---+---+---
+B   board[1][0] | board[1][1] | board[1][2] 
+   ---+---+---
+C   board[2][0] | board[2][1] | board[2][2] )
     
 
 
